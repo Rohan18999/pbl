07d23074fdd254bda2c1d0093cd8093c21514bde
Rohan18999 <161719063+Rohan18999@users.noreply.github.com>
2025-02-24 15:18:28 +0000

    change

--- scripts.js
+++ scripts.js
@@ -1,10 +1,4 @@
-const trackerList = [
-    {
-        type: type,
-        amount: amount,
-        date: date
-    }
-];
+const trackerList = [];
 
 function newExpense(){
     const inputElement = document.querySelector('.js-input')
@@ -16,13 +10,19 @@ function newExpense(){
     const dateInputElement = document.querySelector('.js-date')
     const date = dateInputElement.value
 
-    trackerList.push({
-        type,
-        amount,
-        date
-    })
-
-    /*for(let i = 0; i <trackerList.length(); i++){
+    trackerList.push({ type, amount, date });
+    const html = `<tbody>
+                        <td>
+                            <input class="js-input" type="text">
+                        </td>
+                        <td>
+                            <input class="js-number" type="number" min="0">
+                        </td>
+                        <td>
+                            <input class="js-date" type="date">
+                        </td>
+                </tbody>`
+    /*for(let i = 0; i <trackerList.length; i++){
         const expenseObject = trackerList[i]
         let html = `
         <p>
