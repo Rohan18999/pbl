f0862012fe5d934a889bfc09fadc2dda8df0b93b
Rohan18999 <161719063+Rohan18999@users.noreply.github.com>
2025-02-25 13:31:19 +0000

    changed

--- scripts.js
+++ scripts.js
@@ -1,38 +1,37 @@
-const trackerList = [];
+const trackerList = []
+function newExpense(event){
+    event.preventDefault()
+    const expenseElement = document.getElementById('expense-name')
+    const name = expenseElement.value
 
-function newExpense(){
-    const inputElement = document.querySelector('.js-input')
-    const type = inputElement.value
+    const AmountElement = document.getElementById('expense-amount')
+    const amount = AmountElement.value
 
-    const numberInputElement = document.querySelector('.js-number')
-    const amount = numberInputElement.value
+    const categoryElement = document.getElementById('expense-category')
+    const category =categoryElement.value
 
-    const dateInputElement = document.querySelector('.js-date')
-    const date = dateInputElement.value
+    const dateElement = document.getElementById('expense-date')
+    const date = dateElement.value
 
-    trackerList.push({ type, amount, date });
-    const html = `<tbody>
-                        <td>
-                            <input class="js-input" type="text">
-                        </td>
-                        <td>
-                            <input class="js-number" type="number" min="0">
-                        </td>
-                        <td>
-                            <input class="js-date" type="date">
-                        </td>
-                </tbody>`
-    /*for(let i = 0; i <trackerList.length; i++){
-        const expenseObject = trackerList[i]
-        let html = `
-        <p>
-            ${expenseObject}
-            <button>Delete</button>
-        </p>
-        `
-       
-    }*/
-
-    console.log(trackerList)
-
+    trackerList.push(
+        {
+            name,
+            amount,
+            category,
+            date
+        }
+    )
+    const list = document.getElementById('expense-list')
+    list.insertAdjacentHTML("beforeend", `
+        <tr>
+            <td>${name}</td>
+            <td>${amount}</td>
+            <td>${category}</td>
+            <td>${date}</td>
+            <td>
+                <button>Edit</button>
+                <button>Delete</button>
+            </td>
+        </tr>
+        `);
 }
